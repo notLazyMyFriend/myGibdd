23999fad46027eb9180763ce246a2dc6884a0f13
Shamil Garipov <Shamil0220@rambler.ru>
2020-02-21 16:45:11 +0500

    ses1

---
+++ session_1/debug_dll/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VIN_LIB;
+using System.Text.RegularExpressions;
+
+namespace debug_dll
+{
+    class Program
+    {
+        static void Main(string[] args)                     //10
+        {                                                //7  |
+            Console.WriteLine(new Class1().GetTransportYear("JHMCM56557C404453"));
+            Console.WriteLine(new Class1().GetVINCountry("JHMCM56557C404453"));
+            Console.WriteLine(new Class1().CheckVIN("JHMCM56557C404453"));
+            Console.ReadLine();
+        }
+    }
+}
